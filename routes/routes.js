85bd7ae752f0a3d96a4ea114263e319450f5ad92
Henrik Johansson <henrik.johansson712@gmail.com>
2021-12-07 11:25:57 +0100

    Update request handling

--- routes/routes.js
+++ routes/routes.js
@@ -71,14 +71,22 @@ function config_routes(config) {
       delete query.access_token;
     }
 
-    const request_response = await request(req.method,backend_url)
-      .set(auth)
-      .query(query)
-      .send(req.body)
-      .then(utils.backend_success_callback)
-      .catch(utils.backend_response_error_callback);
+    let filter = {};
+    if ("filter" in req.headers) {
+      filter = {filter: req.headers.filter};
+    }
 
-    res.send(request_response.body);
+    try {
+      const request_response = await request(req.method, backend_url)
+        .set(auth)
+        .set(filter)
+        .query(query)
+        .send(req.body);
+
+      res.status(request_response.status).send(request_response.body);
+    } catch (err) {
+      res.status(err.status).send(JSON.parse(err.response.text));
+    }
   }
 
 }
